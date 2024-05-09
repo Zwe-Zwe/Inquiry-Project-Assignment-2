25bd34671ab9cec9a7c80e9e3d9b9946706de991
William <paingchan211@gmail.com>
2024-05-09 23:45:25 +0800

    connection changes

--- connection.php
+++ connection.php
@@ -6,8 +6,6 @@ $username = "root";
 $password = "";
 
 // Create connection
-//The mysqli_connect() function attempts to open a connection to the MySQL Server 
-//running on host which can be either a host name or an IP address. 
 $conn = mysqli_connect($servername, $username, $password,"",3307); // 3307 is the port number
 
 // Check connection
