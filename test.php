0821bf09d567dc4054b2005bbb95fdeca6bf16e2
Zwe Zwe <zwehtetzaw777@gmail.com>
2024-05-18 14:55:16 +0800

    Management View UI updated

--- test.php
+++ test.php
@@ -98,15 +98,163 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="../styles/style.css" />
     <style>
+        body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+.container {
+    display: flex;
+    width: 100%;
+}
+
+.sidebar {
+    width: 200px;
+    height: 100vh;
+    background-color: #f4f4f4;
+    padding: 20px;
+}
+
+.sidebar .logo img{
+    width: 50%;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    margin-left: 25%;
+}
+
+.sidebar nav ul {
+    list-style: none;
+    padding: 0;
+}
+
+.sidebar nav ul li {
+    margin-bottom: 30px;
+}
+
+.sidebar nav ul li a {
+    text-decoration: none;
+    color: #333;
+    font-size: 20px;
+}
+
+.sidebar nav ul li.active a {
+    font-weight: bold;
+}
+
+main {
+    flex: 1;
+    padding: 20px;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.search-container {
+    display: flex;
+    align-items: center;
+}
+
+.search-container input {
+    padding: 5px;
+    margin-right: 10px;
+}
+
+.search-container button {
+    padding: 5px 10px;
+    margin-right: 10px;
+}
+
+.user-icon img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+
+#table_top {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.sort_logout {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: white;
+    color: black;
+    border-radius: 10px;
+    transition: 0.3s all ease;
+    border: 1px solid black ;
+    text-decoration: none;
+}
+
+#logout:hover{
+    background-color: #333;
+    color: white;
+    border: 1px solid #333 ;
     
+}
+
+.user-management {
+    margin-top: 20px;
+}
+
+.user-management h1 {
+    margin-bottom: 20px;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table th, table td {
+    border: 1px solid #ddd;
+    padding: 10px;
+    text-align: left;
+}
+
+table th {
+    background-color: #f4f4f4;
+}
+
+table .status.active {
+    color: green;
+}
+
+table .status.inactive {
+    color: red;
+}
+
+.pagination {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.pagination a {
+    margin: 0 5px;
+    text-decoration: none;
+    color: #333;
+}
+
+.pagination span {
+    margin: 0 5px;
+}
 
     </style>
 </head>
 <body>
-    <section id="management">
     <div class="container">
         <aside class="sidebar">
-            <div class="logo"><img src="../images/logo2.png"></div>
+            <div class="logo"><img src="images/logo2.png"></div>
             <nav>
                 <ul>
                     <li><a href="#">User Management</a></li>
@@ -124,7 +272,7 @@ $result = $conn->query($sql);
             <section class="user-management">
                 <div id="table_top">
                     <h1>User management</h1>
-                    <a class="sort_logout" href="?sort=''">Default</a>  <a class="sort_logout" href="?sort=asc">Sort Ascending</a>  <a class="sort_logout" href="?sort=desc">Sort Descending</a>
+                    <a class="sort_logout" href="?sort=''">Default</a> | <a class="sort_logout" href="?sort=asc">Sort Ascending</a> | <a class="sort_logout" href="?sort=desc">Sort Descending</a>
                     <a class="sort_logout" href="../index.php">Logout</a>
                 </div>    
                 <table>
@@ -150,7 +298,7 @@ $result = $conn->query($sql);
                                 <td>{$row['password']}</td>
                                 <td>
                                     <a id='edit-button' href='index.php?action=edit&id={$row['id']}'>Edit</a>
-                                    <a id='delete-button' href='index.php?action=delete&id={$row['id']}'>Delete</a>
+                                    <a id='delete-btn' href='index.php?action=delete&id={$row['id']}'>Delete</a>
                                 </td>
                             </tr>
                             ";
@@ -185,6 +333,5 @@ $result = $conn->query($sql);
             </section>
         </main>
     </div>
-    </section>
 </body>
 </html>
