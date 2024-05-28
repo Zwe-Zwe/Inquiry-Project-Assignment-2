745fc1d1c88dd7ba562a6179af0ca1e7583c07a4
wng-mchl <163238474+wng-mchl@users.noreply.github.com>
2024-05-28 17:41:10 +0800

    confirm

--- admin/index.php
+++ admin/index.php
@@ -74,6 +74,26 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action'])) {
     }
 }
 
+if (isset($_GET['action']) && ($_GET['action'] == 'delete_confirmation' && isset($_GET['id']))) {
+
+
+    echo "<div id='user-edit' class='pop-up' style='display: flex;'>
+    <div class='pop-up-content'>  
+      <div id='pop-up-header'>
+        <p>Delete Confirmation</p>
+      </div>
+      
+      <br>
+      <a class='close-btn' href='viewenquirytest.php'>&times;</a>
+      <p>Are you sure with deleting?</p>
+      <a id='delete-button' href='viewenquirytest.php?action=delete&id={$row['id']}'>Delete</a>
+      <a class='exit-button' href='viewenquirytest.php'>Back</a>
+
+    </div>
+  </div>";
+}
+
+
 // Default sorting order
 $sortOrder = "ASC"; // Default ascending order
 
