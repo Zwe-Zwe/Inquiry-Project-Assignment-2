b0be5c59d32e69312a9c6b485ec5ea3e73218041
wng-mchl <163238474+wng-mchl@users.noreply.github.com>
2024-05-28 17:03:14 +0800

    viewvolunteers

--- admin/viewvolunteers.php
+++ admin/viewvolunteers.php
@@ -27,8 +27,8 @@
                         <ul>
                             <li><a href="index.php">User Management</a></li>
                             <li><a href="index.php?action=add">Add New User</a></li>
-                            <li><a href="#viewenquirytest.php">Enquiry Forms</a></li>
-                            <li class="active"><a href="#">Volunteer Forms</a></li>
+                            <li><a href="viewenquirytest.php">Enquiry Forms</a></li>
+                            <li class="active"><a href="viewvolunteers.php">Volunteer Forms</a></li>
                             <li><a href="../index.php">Logout</a></li>
                         </ul>
                     </nav>
@@ -38,7 +38,7 @@
 
                     <!-- search and sort goes here -->
                     <div id="top_ui">
-                        <h1>View Enquiries</h1>
+                        <h1>View Volunteers</h1>
 
                         <form>
                             <table>
@@ -63,11 +63,15 @@
                     <!-- padding in between sections -->
                     <table>
                         <tr>
-                            <th class="enquiry_table_header">Name</th>
-                            <th class="enquiry_table_header">Service Type</th>
-                            <th class="enquiry_table_header">Contact Method</th>
-                            <th class="enquiry_table_header">Appointment Option</th>
-                            <th class="enquiry_table_header">Options</th>
+                            <th class="volunteer_table_header">Full Name</th>
+                            <th class="volunteer_table_header">Email</th>
+                            <th class="volunteer_table_header">Phone #</th>
+                            <th class="volunteer_table_header">Address</th>
+                            <th class="volunteer_table_header">Organization</th>
+                            <th class="volunteer_table_header">Full Time / Part Time</th>
+                            <th class="volunteer_table_header">Working Days & Time</th>
+                            <th class="volunteer_table_header">Reason</th>
+                            <th class="volunteer_table_header">Options</th>
                         </tr>
 
                         <?php
@@ -83,10 +87,10 @@
                         }
 
                         if (empty($_GET['search'])) {
-                            $sql = "SELECT * FROM enquiry_information";
+                            $sql = "SELECT * FROM volunteer_information";
                         } else {
                             $filtervalues = $_GET['search'];
-                            $sql = "SELECT * FROM enquiry_information WHERE CONCAT(first_name,last_name,service_type,contact_method,appointment_option) LIKE '%$filtervalues%'";
+                            $sql = "SELECT * FROM volunteer_information WHERE CONCAT(first_name,last_name,email,phone_num,street_address,city_or_town,state,postcode,organization,organization_type,days,time,message) LIKE '%$filtervalues%'";
                         }
 
                         $result = $conn->query($sql);
@@ -99,28 +103,31 @@
                             while ($row = $result->fetch_assoc()) {
                                 echo "<tr>
                                     <td>{$row['first_name']} {$row['last_name']}</td>
-                                    <td>{$row['service_type']}</td>
-                                    <td>{$row['contact_method']}</td>
-                                    <td>{$row['appointment_option']}</td>
-                                    <td>
-                                        <a id='view-button' href='viewenquirytest.php?action=view&id={$row['id']}'>View</a>
-                                        <a id='delete-button' href='viewenquirytest.php?action=delete_confirmation&id={$row['id']}'>Delete</a>
+                                    <td>{$row['email']}</td>
+                                    <td>{$row['phone_num']}</td>
+                                    <td>{$row['street_address']} {$row['postcode']} {$row['city_or_town']} {$row['state']}</td>
+                                    <td>{$row['organization']}</td>
+                                    <td>{$row['organization_type']}</td>
+                                    <td>{$row['days']} {$row['time']}</td>
+                                    <td>{$row['message']}</td>
+                                    <td id='volunteer_options'>
+                                        <a id='view-button' href='viewvolunteers.php?action=view&id={$row['id']}'>View</a>
+                                        <a id='delete-button' href='viewvolunteers.php?action=delete_confirmation&id={$row['id']}'>Delete</a>
                                     </td>
                                 </tr>";
 
                                 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action'])) {
                                     if ($_GET['action'] == 'delete' && isset($_GET['id'])) {
                                         $id = $_GET['id'];
-                                        $sql = "DELETE FROM enquiry_information WHERE id=?";
+                                        $sql = "DELETE FROM volunteer_information WHERE id=?";
                                         $stmt = $conn->prepare($sql);
                                         $stmt->bind_param("i", $id);
                                         $stmt->execute();
-                                        header("Location: viewenquirytest.php");
                                         exit();
                                     } else if ($_GET['action'] == 'view' && isset($_GET['id'])) {
                                         // Handle view form display
                                         $id = $_GET['id'];
-                                        $sql = "SELECT * FROM enquiry_information WHERE id=?";
+                                        $sql = "SELECT * FROM volunteer_information WHERE id=?";
                                         $stmt = $conn->prepare($sql);
                                         $stmt->bind_param("i", $id);
                                         $stmt->execute();
@@ -128,13 +135,16 @@
                                         $row = $result->fetch_assoc();
                                         $fullname = $row['first_name'] . " " . $row['last_name'];
                                         $email = $row['email'];
-                                        $countryCode = $row['countryCode'];
-                                        $phoneNumber = $row['phoneNumber'];
-                                        $service_type = $row['service_type'];
-                                        $contact_method = $row['contact_method'];
-                                        $appointment_option = $row['appointment_option'];
-                                        $appointment_date = $row['appointment_date'];
-                                        $appointment_time = $row['appointment_time'];
+                                        $phone_num = $row['phone_num'];
+                                        $street_address = $row['street_address'];
+                                        $postcode = $row['postcode'];
+                                        $city_or_town = $row['city_or_town'];
+                                        $state = $row['state'];
+                                        $organization = $row['organization'];
+                                        $organization_type = $row['organization_type'];
+                                        $days = $row['days'];
+                                        $time = $row['time'];
+                                        $message = $row['message'];
                                         $show_info = 1;
                                     }
                                 }
@@ -149,10 +159,10 @@
                                       </div>
                                       
                                       <br>
-                                      <a class='close-btn' href='viewenquirytest.php'>&times;</a>
+                                      <a class='close-btn' href='viewvolunteers.php'>&times;</a>
                                       <p>Are you sure with deleting?</p>
-                                      <a id='delete-button' href='viewenquirytest.php?action=delete&id={$row['id']}'>Delete</a>
-                                      <a class='exit-button' href='viewenquirytest.php'>Back</a>
+                                      <a id='delete-button-v' href='viewvolunteers.php?action=delete&id={$row['id']}'>Delete</a>
+                                      <a class='exit-btn' href='viewvolunteers.php'>Exit</a>
                           
                                     </div>
                                   </div>";
@@ -165,27 +175,30 @@
                                 echo " <div id='user-edit' class='pop-up' style='display: flex;'>
                                   <div class='pop-up-content'>
                                     <div id='pop-up-header'>
-                                      <p>Full Enquiry</p>
+                                      <p>Full Volunteers</p>
                                     </div>
                                     <br>
-                                    <a class='close-btn' href='viewenquirytest.php'>&times;</a>
+                                    <a class='close-btn' href='viewvolunteers.php'>&times;</a>
                       
                                     <p>Name: <?php echo $fullname; ?></p>
                                     <p>E-mail: <?php echo $email; ?></p>
-                                    <p>Country Code: <?php echo $countryCode; ?></p>
-                                    <p>Phone Number: <?php echo $phoneNumber; ?></p>
-                                    <p>Service Type: <?php echo $service_type; ?></p>
-                                    <p>Contact Method: <?php echo $contact_method; ?></p>
-                                    <p>Appointment Option: <?php echo $appointment_option; ?></p>
-                                    <p>Appointment Date: <?php echo $appointment_date; ?></p>
-                                    <p>Appointment Time:<?php echo $appointment_time; ?></p>
+                                    <p>Phone Number: <?php echo $phone_num; ?></p>
+                                    <p>Street Address: <?php echo $street_address; ?></p>
+                                    <p>Postcode: <?php echo $postcode; ?></p>
+                                    <p>City/Town: <?php echo $city_or_town; ?></p>
+                                    <p>State: <?php echo $state; ?></p>
+                                    <p>Organization:<?php echo $organization; ?></p>
+                                    <p>Organization Type:<?php echo $organization_type; ?></p>
+                                    <p>Working Days:<?php echo $days; ?></p>
+                                    <p>Working Time:<?php echo $time; ?></p>
+                                    <p>Message:<?php echo $message; ?></p>
                       
                                   </div>
                                 </div>";
                             }
 
                         } else {
-                            echo "<tr><td colspan='5'>No record found. Please search again, or reset.</td></tr>";
+                            echo "<tr><td colspan='9'>No record found. Please search again, or reset.</td></tr>";
                         }
                         ?>
 
@@ -194,7 +207,7 @@
                     <?php
                     if (isset($_GET['action']) && ($_GET['action'] == 'view' && isset($_GET['id']))) {
                         $id = $_GET['id'];
-                        $sql = "SELECT * FROM enquiry_information WHERE id=?";
+                        $sql = "SELECT * FROM volunteer_information WHERE id=?";
                         $stmt = $conn->prepare($sql);
                         $stmt->bind_param("i", $id);
                         $stmt->execute();
@@ -203,29 +216,35 @@
                             $row = $result->fetch_assoc();
                             $fullname = $row['first_name'] . " " . $row['last_name'];
                             $email = $row['email'];
-                            $countryCode = $row['countryCode'];
-                            $phoneNumber = $row['phoneNumber'];
-                            $service_type = $row['service_type'];
-                            $contact_method = $row['contact_method'];
-                            $appointment_option = $row['appointment_option'];
-                            $appointment_date = $row['appointment_date'];
-                            $appointment_time = $row['appointment_time'];
+                            $phone_num = $row['phone_num'];
+                            $street_address = $row['street_address'];
+                            $postcode = $row['postcode'];
+                            $city_or_town = $row['city_or_town'];
+                            $state = $row['state'];
+                            $organization = $row['organization'];
+                            $organization_type = $row['organization_type'];
+                            $days = $row['days'];
+                            $time = $row['time'];
+                            $message = $row['message'];
                             echo "<div id='user-edit' class='pop-up' style='display: flex;'>
                                     <div class='pop-up-content'>
                                         <div id='pop-up-header'>
-                                            <p>Full Enquiry</p>
+                                            <p>Full Volunteers</p>
                                         </div>
                                         <br>
-                                        <a class='close-btn' href='viewenquirytest.php'>&times;</a>
-                                        <p>Name: $fullname</p>
-                                        <p>E-mail: $email</p>
-                                        <p>Country Code: $countryCode</p>
-                                        <p>Phone Number: $phoneNumber</p>
-                                        <p>Service Type: $service_type</p>
-                                        <p>Contact Method: $contact_method</p>
-                                        <p>Appointment Option: $appointment_option</p>
-                                        <p>Appointment Date: $appointment_date</p>
-                                        <p>Appointment Time: $appointment_time</p>
+                                        <a class='close-btn' href='viewvolunteers.php'>&times;</a>
+                                        <p>Name: <?php echo $fullname; ?></p>
+                                        <p>E-mail: <?php echo $email; ?></p>
+                                        <p>Phone Number: <?php echo $phone_num; ?></p>
+                                        <p>Street Address: <?php echo $street_address; ?></p>
+                                        <p>Postcode: <?php echo $postcode; ?></p>
+                                        <p>City/Town: <?php echo $city_or_town; ?></p>
+                                        <p>State: <?php echo $state; ?></p>
+                                        <p>Organization:<?php echo $organization; ?></p>
+                                        <p>Organization Type:<?php echo $organization_type; ?></p>
+                                        <p>Working Days:<?php echo $days; ?></p>
+                                        <p>Working Time:<?php echo $time; ?></p>
+                                        <p>Message:<?php echo $message; ?></p>
                                     </div>
                                 </div>";
                         }
